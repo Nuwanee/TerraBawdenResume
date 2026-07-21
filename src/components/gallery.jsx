c94f8d6dc09303a07f3e2885fa93e7c873a90e03
Terra Bawden <tnuwaneeb@gmail.com>
2026-07-21 09:54:34 -0700

    Put photos in gallery

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -6,7 +6,22 @@ export default function ImageGallery() {
 
         <div className={"gallery"}>
             <img className={"gallery-img"} src={"portrait2.jpg"} alt={"Gallery Image"} />
+            <img className={"gallery-img"} src={"bubbles.jpg"} alt={"Gallery Image"} />
             <img className={"gallery-img"} src={"portrait3.jpg"} alt={"Gallery Image"} />
+            <img className={"gallery-img"} src={"bridge.jpg"} alt={"Gallery Image"} />
+            <img className={"gallery-img"} src={"cherry-tree.jpg"} alt={"Gallery Image"} />
+            <img className={"gallery-img"} src={"deadlift.jpg"} alt={"Gallery Image"} />
+            <img className={"gallery-img"} src={"ocean.jpg"} alt={"Gallery Image"} />
+            <img className={"gallery-img"} src={"flex-pose.jpg"} alt={"Gallery Image"} />
+            <img className={"gallery-img"} src={"jeep.jpg"} alt={"Gallery Image"} />
+            <img className={"gallery-img"} src={"mist.jpg"} alt={"Gallery Image"} />
+            <img className={"gallery-img"} src={"mug.jpg"} alt={"Gallery Image"} />
+            <img className={"gallery-img"} src={"ohio-flower.jpg"} alt={"Gallery Image"} />
+            <img className={"gallery-img"} src={"pittock-pose.jpg"} alt={"Gallery Image"} />
+            <img className={"gallery-img"} src={"rowena.jpg"} alt={"Gallery Image"} />
+            <img className={"gallery-img"} src={"portrait4.jpg"} alt={"Gallery Image"} />
+            <img className={"gallery-img"} src={"tree-painting.jpg"} alt={"Gallery Image"} />
+            <img className={"gallery-img"} src={"wiper-motor.jpg"} alt={"Gallery Image"} />
         </div>
     )
 }
