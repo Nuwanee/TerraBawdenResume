8a4303bf5406f8106e19b798fd16eacdc9e505e8
Terra Bawden <tnuwaneeb@gmail.com>
2026-07-21 08:18:05 -0700

    Add navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
 import './App.css'
 import MyResume from './components/resume_contents.jsx'
 import ImageBanner from './components/header.jsx'
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+
+function Home() {
+    return (
+        <>
+            <ImageBanner className={"website-header"}/>
+            <MyResume/>
+        </>
+
+    )
+}
+
+function Gallery() {
+    return (
+        <>
+            <ImageBanner className={"website-header"}/>
+        </>
+
+    )
+}
 
 function App() {
 
-  return (
-    <>
-        <ImageBanner className={"website-header"}/>
-        <MyResume />
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <nav className={"navbar"}>
+                <Link to={"/"}>Home</Link> | {" "}
+                <Link to={"/gallery"}>Gallery</Link>
+
+            </nav>
+
+            <Routes>
+                <Route path={"/"} element={<Home/>}/>
+                <Route path={"/gallery"} element={<Gallery/>}/>
+            </Routes>
+
+        </BrowserRouter>
+    )
 }
 
 export default App
